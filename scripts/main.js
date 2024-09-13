bb3d4c8bdaa683a74b8e3b7b72dfd134f9f50565
Joseph Heinz <joheb41@student.oaisd.org>
2024-09-13 13:32:27 +0000

    completed main page

--- scripts/main.js
+++ scripts/main.js
@@ -5,3 +5,14 @@ confetti.setSize(1);
 confetti.setPower(25);
 confetti.setFade(false);
 confetti.destroyTarget(false);
+
+let nameClicks = 0;
+
+document.querySelector("#name").addEventListener("click", () => {
+    
+    nameClicks++;
+    if(nameClicks === 100) {
+        alert("You must really like confetti... clicking my name 100 times");
+    }
+
+});
